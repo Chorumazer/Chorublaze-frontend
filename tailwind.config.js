a23522688dd19343ce3f7727853c659a478db8cb
Bernardo <bernardogualberto8@gmail.com>
2024-01-28 14:04:25 -0300

    feat: install tailwind and configure

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./app.vue",
+    "./error.vue",
+  ],
+  theme: {
+    extend: {
+      "background": "#0F1923",
+      "backgroundGray": "#253340",
+      "backgroundDarkGray": "#1A242D",
+      "accent": "#A81E35",
+      "accentAlt": "#04D17A",
+      "functional": "#ACB1CD"
+    },
+  },
+  plugins: [],
+}
